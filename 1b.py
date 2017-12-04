2b25e0911b21704c0b3fc8cf22041948763cf3bc
Bucsánszki Tamás <bucsitomi+git@gmail.com>
2017-12-04 14:42:59 +0100

    1b done

--- 1b.py
+++ 1b.py
@@ -5,11 +5,12 @@ for i in range(len(be)):
 	be[i]=int(be[i])
 
 sum=0
+limit=int(len(be)/2)
+
 for i in range(len(be)):
-	if i==len(be)-1:
-		kov=0
-	else:
-		kov=i+1
+	kov=i+limit
+	if kov>limit:
+		kov-=len(be)
 	if be[i]==be[kov]:
 		sum+=be[i]
 print("A bemenet hossza:", len(be))
